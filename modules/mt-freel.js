1c793a881e83e5879733d00157569e7af26e2927
Oleksandr <lysenkoa86@gmail.com>
2019-05-23 15:06:21 +0300

    Commit

---
+++ modules/mt-freel.js
@@ -0,0 +1,9 @@
+const freel = {
+    prop1: 'val1',
+    prop2: 'val2',
+    method1(){
+        console.log(`method1 and prop1=${this.prop1}`);
+    }
+};
+
+export default freel;
---
+++ modules/scripts.js
@@ -0,0 +1,8 @@
+import { print as p, log } from './text-helpers';
+//import * as funcs from './text-helpers';
+import freel from './mt-freel';
+
+p('printing a message');
+log('logging a message');
+
+freel.method1();
