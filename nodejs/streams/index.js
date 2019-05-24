a64060018014d270f6f32af76c63b19e1d32837c
Oleksandr <lysenkoa86@gmail.com>
2019-05-24 14:27:22 +0300

    Commit

---
+++ nodejs/streams/index.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+
+    if (input !== null) {
+        // Ехо-вивод текста
+        process.stdout.write(input);
+        var command = input.trim();
+        if (command == 'exit') {
+            process.exit(0);
+        }
+    }
+});
