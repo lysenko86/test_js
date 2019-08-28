47498b7cdfdb058355791a658f233178d6f6e3c2
Oleksandr <lysenko86@i.ua>
2019-08-28 07:41:21 +0300

    Commit

--- react/app_color_list_lifecycle/src/components/ColorList.js
+++ react/app_color_list_lifecycle/src/components/ColorList.js
@@ -3,7 +3,7 @@ import Color from './Color'
 
 const ColorList = ({ colors=[], onRate=f=>f, onRemove=f=>f }) =>
     <div className="color-list">
-        {(colors.length === 0) ?
+        {colors.length === 0 ?
             <p>No Colors Listed. (Add a Color)</p> :
             colors.map(color =>
                 <Color key={color.id} {...color}
