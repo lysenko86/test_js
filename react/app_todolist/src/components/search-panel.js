0df3d1aef4e3a6aa41edeab24b2cdcdbf3ffc92e
Oleksandr <lysenkoa86@gmail.com>
2019-09-26 18:53:19 +0300

    Commit

--- react/app_todolist/src/components/search-panel.js
+++ react/app_todolist/src/components/search-panel.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import './search-panel.css';
 
-const SearchPanel = () => <input placeholder="search" />;
+const SearchPanel = () => {
+    return (
+        <input
+            type="text"
+            className="form-control search-input"
+            placeholder="search" />
+    );
+};
 
 export default SearchPanel;
