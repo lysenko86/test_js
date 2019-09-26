48e4f39ee0c2653319850932cf03a9da03697024
Oleksandr <lysenkoa86@gmail.com>
2019-09-26 15:50:38 +0300

    Commit

--- react/app_todolist/src/components/todo-list-item.js
+++ react/app_todolist/src/components/todo-list-item.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const TodoListItem = () => <span>Drink coffee</span>;
+const TodoListItem = ({ label, important=false }) => {
+    const style = {
+        color: important ? 'tomato' : 'black'
+    };
+    return <span style={ style }>{ label }</span>
+};
 
 export default TodoListItem;
