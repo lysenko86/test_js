149095294c4daa7ed2166cb3f1e1567a5fa0b815
Oleksandr <lysenkoa86@gmail.com>
2019-10-01 13:29:30 +0300

    Commit

--- react/app_todolist/src/components/todo-list-item/todo-list-item.js
+++ react/app_todolist/src/components/todo-list-item/todo-list-item.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important=false }) => {
-    const style = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-    };
-    return (
-        <span className="todo-list-item">
-            <span
-                className="todo-list-item-label"
-                style={ style }>{ label }</span>
-            <button
-                type="button"
-                className="btn btn-outline-success btn-sm float-right"
-            ><i className="fas fa-exclamation" /></button>
-            <button
-                type="button"
-                className="btn btn-outline-danger btn-sm float-right"
-            ><i className="far fa-trash-alt" /></button>
-        </span>
-    );
-};
+export default class TodoListItem extends Component {
+    constructor () {
+        super();
+    }
+    
+    onLabelClick () {
+        console.log(`Done: ${this.props.label}`);
+    }
 
-export default TodoListItem;
+    render () {
+        const { label, important=false } = this.props;
+        const style = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
+        return (
+            <span className="todo-list-item">
+                <span
+                    className="todo-list-item-label"
+                    style={ style }
+                    onClick={ this.onLabelClick.bind(this) }>{ label }</span>
+                <button
+                    type="button"
+                    className="btn btn-outline-success btn-sm float-right"
+                ><i className="fas fa-exclamation" /></button>
+                <button
+                    type="button"
+                    className="btn btn-outline-danger btn-sm float-right"
+                ><i className="far fa-trash-alt" /></button>
+            </span>
+        );
+    }
+}
