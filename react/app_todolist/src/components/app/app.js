233432a566eacd700a6a849ab74ef366000502c9
Oleksandr <lysenkoa86@gmail.com>
2019-10-08 06:57:58 +0300

    Commit

--- react/app_todolist/src/components/app/app.js
+++ react/app_todolist/src/components/app/app.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 import './app.css';
 
-const App = () => {
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 }
-    ];
+export default class App extends Component {
+    constructor () {
+        super();
+        this.state = {
+            todoData: [
+                { label: 'Drink Coffee', important: false, id: 1 },
+                { label: 'Make Awesome App', important: true, id: 2 },
+                { label: 'Have a lunch', important: false, id: 3 }
+            ]
+        };
+    }
 
-    return (<div className="todo-app">
-        <AppHeader toDo={ 1 } done={ 3 } />
-        <div className="top-panel d-flex">
-            <SearchPanel />
-            <ItemStatusFilter />
-        </div>
-        <TodoList todos={ todoData } />
-    </div>);
+    deleteItem = (id) => {
+        this.setState(({ todoData }) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+            const newArray = [
+                ...todoData.slice(0, idx),
+                ...todoData.slice(idx + 1)
+            ];
+            return {
+                todoData: newArray
+            };
+        });
+    }
+
+    render () {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={ 1 } done={ 3 } />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <TodoList
+                    todos={ this.state.todoData }
+                    onDeleted={ this.deleteItem } />
+            </div>
+        );
+    }
 };
-
-export default App;
--- react/app_todolist/src/components/todo-list-item/todo-list-item.js
+++ react/app_todolist/src/components/todo-list-item/todo-list-item.js
@@ -4,31 +4,55 @@ import './todo-list-item.css';
 export default class TodoListItem extends Component {
     constructor () {
         super();
-    }
-    
-    onLabelClick () {
-        console.log(`Done: ${this.props.label}`);
+        this.state = {
+            done: false,
+            important: false
+        };
     }
 
+    onLabelClick = () => {
+        this.setState(({ done }) => {
+            return {
+                done: !done
+            }
+        });
+    };
+
+    onMarkImportant = () => {
+        this.setState(({ important }) => {
+            return {
+                important: !important
+            }
+        });
+    };
+
     render () {
-        const { label, important=false } = this.props;
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
+        const { label, onDeleted } = this.props;
+        const { done, important } = this.state;
+        let classNames = 'todo-list-item';
+
+        if (done) {
+            classNames += ' done';
+        }
+
+        if (important) {
+            classNames += ' important';
+        }
+
         return (
-            <span className="todo-list-item">
+            <span className={ classNames }>
                 <span
                     className="todo-list-item-label"
-                    style={ style }
-                    onClick={ this.onLabelClick.bind(this) }>{ label }</span>
+                    onClick={ this.onLabelClick }>{ label }</span>
                 <button
                     type="button"
                     className="btn btn-outline-success btn-sm float-right"
+                    onClick={ this.onMarkImportant }
                 ><i className="fas fa-exclamation" /></button>
                 <button
                     type="button"
                     className="btn btn-outline-danger btn-sm float-right"
+                    onClick={ onDeleted }
                 ><i className="far fa-trash-alt" /></button>
             </span>
         );
