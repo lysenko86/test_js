2716beae2c80781c450395420c33184d3fdd3d9e
Oleksandr <lysenkoa86@gmail.com>
2019-10-22 21:30:15 +0300

    Commit

--- spa/simple_spa/index.js
+++ spa/simple_spa/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 
-router.listen(4000, () => {
+mongoose.connect('mongodb://localhost/muggers-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(bodyParser.json());
+app.use('/api', require('./api'));
+
+app.listen(4000, () => {
     console.log('Server is listening...');
 });
