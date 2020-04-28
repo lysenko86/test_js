255f4c98dfc00d1818c518e6b966eec8c6fd1150
oleksandr_86 <oleksandr.lysenko@indealpro.com>
2020-04-28 16:33:28 +0300

    Commit

--- apps/todolist-hooks/src/components/Notes.js
+++ apps/todolist-hooks/src/components/Notes.js
@@ -4,7 +4,13 @@ export const Notes = ({ notes }) => {
 	return (
 		<ul className="list-group">
 			{notes.map(note => (
-				<li className="list-group-item" key={note.id}>{note.title}</li>
+				<li className="list-group-item note" key={note.id}>
+					<div>
+						<strong>{note.title}</strong>
+						<small>{new Date().toLocaleDateString()}</small>
+					</div>
+					<button type="button" className="btn btn-outline-danger btn-sm">&times;</button>
+				</li>
 			))}
 		</ul>
 	);
