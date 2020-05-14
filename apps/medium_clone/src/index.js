8103afaa15e19513e6716e6c80e130b1f2f12c33
oleksandr_86 <oleksandr.lysenko@indealpro.com>
2020-05-14 10:24:50 +0300

    Commit

--- apps/medium_clone/src/index.js
+++ apps/medium_clone/src/index.js
@@ -1,5 +1,25 @@
+/*
+	source: https://www.udemy.com/course/react-hooks-writing-real-project/
+	stack: create-react-app, react, хуки, авторизація, axios, classnames, query-string
+	start ---> yarn start
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+import Routes from 'routes';
+import TopBar from 'components/topBar';
+
+const App = () => {
+	return (
+		<div>
+			<Router>
+				<TopBar />
+				<Routes />
+			</Router>
+		</div>
+	);
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
