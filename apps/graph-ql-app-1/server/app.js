f9f850182a78928ac351a7859792c744bf2dc84d
Oleksandr <olysenko@reaktivate.com>
2021-04-02 14:23:17 +0300

    Test app with GraphQL

---
+++ apps/graph-ql-app-1/server/app.js
@@ -0,0 +1,17 @@
+// source: https://www.youtube.com/playlist?list=PLNkWIWHIRwMF2sVLwzRef0Cu5kzAOeRcu
+
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('../schema/schema');
+
+const app = express();
+const PORT = 3005;
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
+app.listen(PORT, err => {
+  err ? console.log(err) : console.log('Server started!');
+});
