ddf76c1922d7f61e7df47c7c2fdd2ef650647fbc
Oleksandr <olysenko@reaktivate.com>
2021-04-07 17:59:53 +0300

    rect + typescript

---
+++ TypeScript/react-typescript/src/App.tsx
@@ -0,0 +1,16 @@
+// Source: https://www.youtube.com/watch?v=acO37eSCowc&list=PLNkWIWHIRwMFQBDhZ6HfwO9NL09X3N3Gq&index=1
+
+import React from 'react';
+
+//const Title:React.FC<{title: string}> = ({title, children}) => <h1>{title}{children}</h1>;
+
+type TitleProps = {
+  title: string,
+  test?: string,
+};
+
+const Title = ({title, test = 'test'}: TitleProps) => <h1>{title}{test}</h1>;
+
+const App = () => <Title title="title"/>
+
+export default App;
